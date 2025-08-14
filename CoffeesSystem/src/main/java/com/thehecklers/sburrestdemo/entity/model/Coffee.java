f9d33d444c5e3d42594ae0c6f262a3fad4b04c3b
Jackson Moraes <jacksontadeu@hotmail.com>
2025-08-14 04:52:57 -0300

    :sparkles: feat: Criacao do projeto e estruturacao das pastas

---
+++ CoffeesSystem/src/main/java/com/thehecklers/sburrestdemo/entity/model/Coffee.java
@@ -0,0 +1,32 @@
+package com.thehecklers.sburrestdemo.entity.model;
+
+import com.thehecklers.sburrestdemo.entity.enums.Tipo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 200)
+    private String nome;
+
+    @Column(length = 200)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Tipo tipo;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+}
